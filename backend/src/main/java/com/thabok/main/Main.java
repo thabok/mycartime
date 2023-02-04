5915821e10abd49358f26e232dcf08362a25f01c
Thabo Krick <thabokrick@gmail.com>
2023-02-05 00:07:51 +0100

    the java backend now also serves the frondend

--- backend/src/main/java/com/thabok/main/Main.java
+++ backend/src/main/java/com/thabok/main/Main.java
@@ -1,11 +1,25 @@
 package com.thabok.main;
 
 import com.thabok.webservice.WebService;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		new WebService();
-	}
 
+		Server server = new Server(80);
+
+		ServletContextHandler context = new ServletContextHandler();
+		context.setContextPath("/");
+		context.setResourceBase("frontend");
+		context.addServlet(new ServletHolder(DefaultServlet.class), "/");
+
+		server.setHandler(context);
+		server.start();
+		server.join();
+	}
 }
